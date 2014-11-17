2666e68b144c00641fe6ff2a8abcdc69649d295a
Stefano Schiavi <stefano@bvprojects.org>
2014-11-16 23:32:17 -0800

    add alghorithm optimization
    - main optimization is to iterate through users
        starting at different points. By doing so the
        connections build up more quickly giving more
        opportunities more quickly to people to pair
        with someone new

--- app/models/cohort.rb
+++ app/models/cohort.rb
@@ -3,9 +3,11 @@ class Cohort < ActiveRecord::Base
   has_many :users
   has_many :connections, through: :users
 
+  USERS_ORDER = %i(from_begin reverse from_middle).cycle
+
   def make_clubs n_clubs
     clubs = Array.new(n_clubs) { [] }
-    users.each do |user|
+    self.send(USERS_ORDER.next).each do |user|
       connections_count = clubs.map do |club|
         club.inject(0) do |sum, club_user|
           if found_connection = user.connected?(club_user)
@@ -41,6 +43,19 @@ class Cohort < ActiveRecord::Base
         end
       end
     end
+  end
+
+  def from_middle
+    mid_index = users.count / 2
+    users.offset(mid_index) + users.limit(mid_index)
+  end
+
+  def reverse
+    users.reverse_order
+  end
+
+  def from_begin
+    users
   end
 
 end
